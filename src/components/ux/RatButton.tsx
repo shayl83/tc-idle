e2a71a95129b6083fbb6e3987715a5cddb92cc8d
shayl83 <stacey.garrison@gmail.com>
2019-06-17 23:31:04 -0400

    Update RatButton.tsx

--- src/components/ux/RatButton.tsx
+++ src/components/ux/RatButton.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import {ReactNode} from "react";
 
 interface ButtonProps {
+    //STACEY YOU DUMMY - THIS IS WHERE THE BUG IS, WRONG TYPE
     onClick?: () => void
     children: ReactNode,
     className?: string
